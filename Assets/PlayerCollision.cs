c4a5d13aa86e4adaf8c3442e0131c4eb961ec058
naveen965 <39030430+naveen965@users.noreply.github.com>
2020-09-05 14:18:39 +0530

    Add project

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void onCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log("khkjhkjh");
+    }
+}
---
+++ Assets/PlayerMovemont.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class PlayerMovemont : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        // Debug.Log("Hello, World!");
+        rb.AddForce(0, 200, 500);
+    }*/
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
