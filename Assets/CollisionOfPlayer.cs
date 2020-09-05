5932faff7426ec5ebffa5d5a2c82094037d584e1
naveen965 <39030430+naveen965@users.noreply.github.com>
2020-09-06 00:34:12 +0530

    edit CollisionOfPlayer

---
+++ Assets/CollisionOfPlayer.cs
@@ -0,0 +1,15 @@
+﻿/*using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    void onCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            //movement.enabled = false;
+            Debug.Log("Hello");
+        }
+    }
+}*/
